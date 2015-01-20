50c20fd92d8648fa76291104ec03d4fb3199a140
Parker Ennis <rpennis@ncsu.edu>
2015-01-20 15:36:06 -0500

    I PASSED A TEST

---
+++ currency.rb
@@ -0,0 +1,4 @@
+def currency_type(amount, code)
+  amount_array = amount.split("")
+  code_array = code.split("")
+end
--- currency_test.rb
+++ currency_test.rb
@@ -3,4 +3,14 @@ require 'minitest/pride'
 require './currency'
 
 class CurrencyTest < Minitest::Test
-  
+  def test_truth
+    assert true
+  end
+
+  def test_currency_has_two_parameters
+    assert currency_type("", "")
+  end
+
+
+
+end
